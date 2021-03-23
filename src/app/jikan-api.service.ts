70d991908a81b64ba80ff2ba1bef33b1924b1baf
Li <steven.li@efrei.net>
2021-03-23 20:30:18 +0100

    Jikan - search anime/ get anime by id

--- src/app/jikan-api.service.ts
+++ src/app/jikan-api.service.ts
@@ -4,11 +4,21 @@ import {HttpClient, HttpErrorResponse, HttpResponse} from '@angular/common/http'
   providedIn: 'root'
 })
 export class JikanAPIService {
+  base_url : String  = "https://api.jikan.moe/v3/";
 
   constructor(
     private http : HttpClient
   ) { }
-  random(){
-    return this.http.get("https://api.jikan.moe/v3/anime/37105");
+  
+  getAnimeById(id : number){
+    return this.http.get(this.base_url+"anime/"+id).subscribe(data => console.log(data));
   }
+
+  searchAnimeByName(title : String){
+    var titleSplited = title.split(" ");
+    var titleFormated = titleSplited.join("%20");
+    this.http.get(this.base_url + "search/anime?q="+titleFormated+"&page=1").subscribe(data => console.log(data));
+  }
+
+
 }
