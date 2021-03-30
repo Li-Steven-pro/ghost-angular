1542a7eb0d56db5de5e86b518841cb3b1b8fdad6
Li <steven.li@efrei.net>
2021-03-30 15:22:05 +0200

    Chage file

--- src/app/interface/theme-moe.ts
+++ src/app/interface/theme-moe.ts
@@ -17,26 +17,3 @@ export interface ThemesMoeAnime {
     watchStatus: number
 }
   
-export interface ThemesAnime {
-    mal_id : number,
-    title : Array<string>,
-    cover : string,
-    year : number,
-    season : string,
-    themes : Array<Theme>
-}
-export interface Theme {
-    title : string,
-    theme_id : string,
-    type : string,
-    artist : string,
-    mirrors : Array<Mirror>,
-    notes : string,
-    episodes : string,
-    category : string
-}
-export interface Mirror {
-    quality : string,
-    mirror : string,
-    audio : string
-}
